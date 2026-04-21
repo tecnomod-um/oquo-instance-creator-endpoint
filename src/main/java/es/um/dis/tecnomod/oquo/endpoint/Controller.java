1819e7c8065323710190b27ded9bcde5241f6332
fanavarro <francisco.abad@um.es>
2026-04-21 15:13:24 +0200

    Use request body instead of request param

--- src/main/java/es/um/dis/tecnomod/oquo/endpoint/Controller.java
+++ src/main/java/es/um/dis/tecnomod/oquo/endpoint/Controller.java
@@ -1,19 +1,24 @@
 package es.um.dis.tecnomod.oquo.endpoint;
 
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
 
 import org.apache.jena.rdf.model.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.um.dis.tecnomod.oquo.dto.ObservationInfoDTO;
 import es.um.dis.tecnomod.oquo.dto.ObservationsInfoDTO;
 import es.um.dis.tecnomod.oquo.service.InstanceCreator;
 
 @RestController
 public class Controller {
 	@PostMapping(value="/toOquoObservation", produces = "text/turtle")
-	public String toOquoObservation (@RequestParam("observationsInfo") ObservationsInfoDTO observationsInfo) {
+	public String toOquoObservation (@RequestBody ObservationsInfoDTO observationsInfo) {
 		Model rdfModel = InstanceCreator.createObservations(observationsInfo);
 		StringWriter writer = new StringWriter();
 		rdfModel.write(writer, "TURTLE");
